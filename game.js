36f9b4bb85b4f8404ecdee716cd53e9c9942c3f9
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-02 19:39:07 +0200

    Update game

--- game.js
+++ game.js
@@ -1,18 +1,43 @@
-const readlineSync = require('readline-sync')
+const fs = require('fs')
+const readline = require('readline-sync')
 
 // import random word function
 const { randomWord } = require('./randomWord')
 
-// loop 
+// ------------- while program --------
 let running = true;
 
 while (running) {
+// initialisation 
+let hideWord = randomWord
 
-  if (readlineSync.keyInYNStrict('continue? ')){
-	   console.log(randomWord)
+let wordToFind = []
+
+  for (let i = 0; i < hideWord.length; i++) {
+    wordToFind.push('_')
   }
-  else {
-      console.log('goodbye!')
-      running = false;
+
+  console.log('Le jeu du pendu game')
+
+  let lifeCount = 10
+
+  while (lifeCount && wordToFind.includes('_')) {
+
+    let letter = ''
+    console.log(`vie restant : ${lifeCount}`)
+    console.log(`Le mot : ${wordToFind.join(' ')}`)
+
+    letter = readline.question('La lettre : ').toLowerCase()
+
+    for (let i = 0; i < hideWord.length; i++) {
+      hideWord[i] === letter ? wordToFind[i] = letter : '' 
+    }
+
+    if (hideWord.includes(letter)) {
+      console.log('Tu es dans le bon chemin garçon')
+    } else {
+      lifeCount--
+      console.log(`C\'est pas la bonne lettre`)
+    }
   }
 }
