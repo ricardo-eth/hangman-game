ebbbdf321442fb6f4a849ff881a0f7b11fd84d51
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-02 19:42:11 +0200

    Update

--- randomWord.js
+++ randomWord.js
@@ -12,7 +12,6 @@ const DICT_FILE = 'dict.txt'
 const dicStr = fs.readFileSync(DICT_FILE, 'utf-8')
 const dicTab = dicStr.split('\n')
 
-
 // ----------------- random word on file --------------
 const selectedWord = dicTab
 
