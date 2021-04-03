7f4c9dd52e93b7082bc316f71af3d325ecae2d39
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-03 23:54:01 +0200

    Update

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ const readline = require('readline-sync')
 // import random word function
 const { randomWord } = require('./randomWord')
 
+const { hangmanPics } = require('./hangmanPics')
+
 // ------------- while program --------
 let running = true;
 
@@ -20,24 +22,53 @@ while (running) {
   console.log('Le jeu du pendu game')
 
   let lifeCount = 10
+  let hangmanPicsFrame = 0
+
+
+
+
+
+
 
   while (lifeCount && wordToFind.includes('_')) {
 
+    console.log(hangmanPics[hangmanPicsFrame])
+
     let letter = ''
-    console.log(`vie restant : ${lifeCount}`)
+    console.log(`Nombre de vie : ${lifeCount}`)
+
+    if (lifeCount < 2){
+      console.log('Attention il vous reste qu\'une seule vie !!');
+    }
+
     console.log(`Le mot : ${wordToFind.join(' ')}`)
 
     letter = readline.question('La lettre : ').toLowerCase()
 
     for (let i = 0; i < wordHide.length; i++) {
-      wordHide[i] === letter ? wordToFind[i] = letter : '' 
+      wordHide[i].toLowerCase() === letter ? wordToFind[i] = letter : '' 
     }
 
     if (wordHide.includes(letter)) {
-      console.log('Tu es sur le bon chemin garçon')
+      console.log('Tu es sur le bon chemin')
     } else {
       lifeCount--
-      console.log(`C\'est pas la bonne lettre attention !!`)
+      hangmanPicsFrame++
+      console.log('outch');
     }
+  }
+
+
+
+  if (lifeCount === 0) {
+    console.log('GAME OVER !')
+    console.log(`Word is : ${wordHide}\n\n`)
+    let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
+    choice === 'y' ? '' : process.exit(1)
+  } else {
+    console.log('YOU WIN !!!')
+    console.log(`Vous avez trouvez le mot : ${wordHide} !!!!!\n\n`)
+    let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
+    choice === 'y' ? '' : process.exit(1)
   }
 }
