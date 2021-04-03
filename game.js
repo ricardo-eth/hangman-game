8026e0bfc0225a07090c9c74935544445124d5d8
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-04 00:32:13 +0200

    Update

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const readline = require('readline-sync')
+const chalk = require('chalk');
 
 // import random word function
 const { randomWord } = require('./randomWord')
@@ -19,55 +20,60 @@ while (running) {
     wordToFind.push('_')
   }
 
-  console.log('Le jeu du pendu game')
-
-  let lifeCount = 10
+  console.log(chalk.blue('==================================================='))
+  console.log(chalk.blue('Le jeu du pendu game'))
+  console.log(chalk.blue('==================================================='))
+  let lifeCount = 7
   let hangmanPicsFrame = 0
 
-
-
-
-
-
-
   while (lifeCount && wordToFind.includes('_')) {
-
-    console.log(hangmanPics[hangmanPicsFrame])
-
     let letter = ''
-    console.log(`Nombre de vie : ${lifeCount}`)
+
+    console.log(`Nombre de vie : ${lifeCount} \n ${hangmanPicsFrame == 6 ? chalk.red(hangmanPics[hangmanPicsFrame]) : hangmanPics[hangmanPicsFrame]}`)
+
+    console.log(chalk.blue('==================================================='))
 
     if (lifeCount < 2){
-      console.log('Attention il vous reste qu\'une seule vie !!');
+      console.log(chalk.red('==================================================='))
+      console.log(chalk.red('Attention il vous reste qu\'une seule vie !!'))
+      console.log(chalk.red('==================================================='))
     }
 
     console.log(`Le mot : ${wordToFind.join(' ')}`)
-
+    console.log(chalk.blue('==================================================='))
     letter = readline.question('La lettre : ').toLowerCase()
 
     for (let i = 0; i < wordHide.length; i++) {
-      wordHide[i].toLowerCase() === letter ? wordToFind[i] = letter : '' 
+      wordHide[i] === letter ? wordToFind[i] = letter : '' 
     }
 
     if (wordHide.includes(letter)) {
-      console.log('Tu es sur le bon chemin')
+      console.log(chalk.green('==================================================='))
+      console.log(chalk.green('Tu es sur le bon chemin !'))
+      console.log(chalk.green('==================================================='))
     } else {
       lifeCount--
       hangmanPicsFrame++
-      console.log('outch');
+      console.log(chalk.red('==================================================='))
+      console.log(chalk.red('Mauvaise lettre...'))
+      console.log(chalk.red('==================================================='))
     }
   }
 
-
-
   if (lifeCount === 0) {
-    console.log('GAME OVER !')
+    console.log(chalk.red(' '))
+    console.log(chalk.red('==================================================='))
+    console.log(chalk.red('==================================================='))
+    console.log(chalk.red('GAME OVER !'))
+    console.log(chalk.red('==================================================='))
+    console.log(chalk.red('==================================================='))
+    console.log(chalk.red('==================================================='))
     console.log(`Word is : ${wordHide}\n\n`)
     let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
     choice === 'y' ? '' : process.exit(1)
   } else {
     console.log('YOU WIN !!!')
-    console.log(`Vous avez trouvez le mot : ${wordHide} !!!!!\n\n`)
+    console.log(`Vous avez trouvé le mot : ${wordHide} !!!!!\n\n`)
     let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
     choice === 'y' ? '' : process.exit(1)
   }
